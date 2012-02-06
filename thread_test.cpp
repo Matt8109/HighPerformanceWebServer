53235a2b8d9d6d6c96514fe3740d50e35c3cc299
Matthew Mancuso <matt8109@gmail.com>
2012-02-05 22:47:44 -0500

    Just pushing to a new computer.

--- thread_test.cpp
+++ thread_test.cpp
@@ -1,11 +1,39 @@
+#include <pthread.h>
+
 #include "test_unit.hpp"
 #include "thread.hpp"
+#include "lock.hpp"
 
 namespace {
+using base::Mutex;
 
-TEST(Group, Case) {
+TEST(Single, BitFlip) {
+  TestThread test;
+
+  
   EXPECT_TRUE(true);
 }
+
+struct TestThread {
+  int count;
+  bool hit;
+  Mutex sync_root;
+
+  TestThread() {
+    count = 0;
+    hit = false;
+  }
+
+  void Hit() {
+    hit = true;
+  }
+
+  void Increase() {
+    sync_root.lock();
+    count++;
+    sync_root.unlock();
+  }
+};
 
 } // unnammed namespace
 
