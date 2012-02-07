562a6255c1209d7e386aa848d1f8ec9d89a50895
Matthew Mancuso <matt8109@gmail.com>
2012-02-06 21:51:02 -0500

    Minor update, but weird bug.

--- thread_test.cpp
+++ thread_test.cpp
@@ -22,7 +22,29 @@ TEST(Single, BitFlip) {
   pthread_join(my_thread, NULL);
   
   EXPECT_TRUE(test.is_hit);
+  EXPECT_EQ(test.count, 0);
 }
+
+TEST(Multiple, Count) {
+  const int count = 1; // The number of threads to run
+  TestThread test;
+  pthread_t threads[count];
+
+  Callback<void>* thread_method_one = 
+      makeCallableMany(&TestThread::increase, &test);
+
+  for (int i = 0; i < count; i++)
+    threads[count] = makeThread(thread_method_one);
+
+  for (int i = 0; i < count; i++)
+    pthread_join(threads[count], NULL);
+
+  delete thread_method_one; // Delete callback after all threads finish
+
+  EXPECT_EQ(test.count, count);  
+}
+    
+
 
 } // unnammed namespace
 
