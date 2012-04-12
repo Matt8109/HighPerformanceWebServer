6094ae24a5a471c8bd07ee45ae2e0625f268c411
Matthew Mancuso <matt8109@gmail.com>
2012-04-12 00:02:04 -0400

    Some updates to the analysis document after running tests.

--- request_stats.hpp
+++ request_stats.hpp
@@ -1,17 +1,13 @@
 #ifndef MCP_REQUEST_STATS_HEADER
 #define MCP_REQUEST_STATS_HEADER
 
-#define SLOTS 1000
-
 #include <inttypes.h>
 #include <string>
 
-#include "server_stat_buffer.hpp"
 #include "ticks_clock.hpp"
 
 namespace base {
 
-using base::ServerStatBuffer;
 using base::TicksClock;
 using std::string;
 
@@ -47,18 +43,48 @@ public:
 
   // Accessors (for testing)
 
-  uint64_t ticksPerSlot() const { return ticks_per_slot_; }
+  uint64_t ticksPerSlot() const { return 0; }
 
 private:
-  int const num_threads_;
-  ServerStatBuffer** stat_buffers_;
-  uint64_t const ticks_per_slot_;
+  const int num_threads_;
 
-  // Non-copyable, non-assignable
-  RequestStats(const RequestStats&);
-  RequestStats& operator=(const RequestStats&);
+  // Number of slots we divide our second in. We use slotting in a
+  // circular structure to allow for a rolling window of 1 second.
+  static const uint32_t kNumSlots = 10;
+
+  // Given that we'll split one second in 'kNumSlots', how many
+  // ticks do we have in one slot.
+  static const uint64_t kTicksPerSlot;
+
+  // We'll pad each instance of 'Count' below assuming the 64 bytes
+  // cache line.
+  static const int kLineSizeInBytes = 64;
+
+  // A circular vector of accumulators that record how many requests
+  // were received in a give slot of time, during a 1 second rolling
+  // window of time.
+  struct Counts {
+    uint32_t base_pos;              // "now" slot in the val array (circular)
+    uint64_t base_tick;             // ticks when "now" started
+    uint32_t val[kNumSlots];        // partial counts of requests
+
+    uint8_t pad[kLineSizeInBytes];  // pad to avoid false sharing of lines
+  };
+  Counts* counts_;                  // owned here
+
+  // Helpers
+
+  // Returns the next position to 'p' in the circular vector.
+  uint32_t incPos(uint32_t p) const { return (p + 1) % kNumSlots; }
+
+  // Returns the position a request arrived at tick 't' should occupy.
+  uint32_t posForTick(uint64_t t) const { return t/kTicksPerSlot % kNumSlots; }
+
+  // Returns the tick previous to tick 't'  that would fall right in the
+  // start of a slot.
+  uint64_t roundTick(uint64_t t) const { return t - (t % kTicksPerSlot); }
 };
 
-}  // namespace http
+}  // namespace base
 
 #endif // MCP_REQUEST_STATS_HEADER
